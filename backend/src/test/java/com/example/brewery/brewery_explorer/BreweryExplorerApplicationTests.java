c53f0eac838b467633af87ec9fb3c9bbf72ff7d4
Lorenzo Matterazzo <lorenzo.matterazzo@lynxspa.com>
2024-07-20 09:10:27 +0200

    Initial project setup with Docker - Set up backend using Spring Boot - Set up frontend using Angular - Configured PostgreSQL database with Docker and Docker Compose - Added .env file for environment variables used in Docker Compose - Added Dockerfiles for backend, frontend, and database services - Created initial application properties and logging configuration for backend - Set up frontend assets and initial components - Added docker-compose.yml for multi-container orchestration

---
+++ backend/src/test/java/com/example/brewery/brewery_explorer/BreweryExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brewery.brewery_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
