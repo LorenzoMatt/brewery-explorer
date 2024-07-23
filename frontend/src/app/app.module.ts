26f79264a8c9c3c070e9ce37592995a17316405c
Lorenzo Matterazzo <lorenzo.matterazzo@lynxspa.com>
2024-07-24 00:55:14 +0200

    Removed deprecated login and register components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,23 +9,19 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { AuthGuard } from './guards/auth.guard';
+import { AuthComponent } from './pages/auth/auth.component';
 import { BreweryDetailComponent } from './pages/brewery-detail/brewery-detail.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { SearchBreweriesComponent } from './pages/search-breweries/search-breweries.component';
 import { AuthInterceptor } from './services/auth.interceptor';
 import { AuthService } from './services/auth.service';
 import { BreweryService } from './services/brewery.service';
-import { AuthComponent } from './pages/auth/auth.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        LoginComponent,
-        RegisterComponent,
         PaginationComponent,
         BreweryListComponent,
         HeaderComponent,
